dfeaf41b91377221f10ceea3e3eeab8fa051fb50
dimassagngsptr <dimasageng58@gmail.com>
2024-06-28 20:16:02 +0700

    fc

---
+++ MyFirstClass.java
@@ -0,0 +1,8 @@
+public class MyFirstClass {
+static void myMethod(String name, int age){
+  System.out.println("My name is " + name + " and my age is" + age);
+}
+  public static void main(String[] args) {
+    myMethod("Dimas", 23);
+  }
+}
